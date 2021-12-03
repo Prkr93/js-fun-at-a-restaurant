f0e532b98ffddbc50533972995449255d29d4d20
Dillon Parker <prkr93@gmail.com>
2021-12-03 10:02:24 -0700

    Complete order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,44 @@
+function takeOrder(orderObj, deliveryArray) {
+  if(deliveryArray.length < 3){
+    deliveryArray.push(orderObj);
+  }
+  return deliveryArray;
+}
+
+//////////////??Why does this work?? Is "index" some sort of keyword in javascript?
+function refundOrder(orderNum, deliveryOrders) {
+  deliveryOrders.forEach((order, index) => {
+    if(order.orderNumber === orderNum) {
+      deliveryOrders.splice(index, 1);
+    }
+  });
+}
+
+function listItems(deliveryOrders) {
+  var items = [];
+  deliveryOrders.forEach((order, index) => {
+    if(index < deliveryOrders.length - 1) {
+      items.push(order.item);
+    } else { items.push(order.item) }
+  });
+  return items.join(', ');
+}
+
+// Why do we have to return the variable, instead of returning true within the if statement, and returning false outside of the for loop?
+function searchOrder(deliveryOrders, item) {
+  var isOrdered = false;
+  deliveryOrders.forEach((order, index) => {
+    if(item == order.item){
+      isOrdered = true;
+    }
+  });
+  return isOrdered;
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
