c7488deb0bd43fa07eb678f59001a7be31974acf
Dillon Parker <prkr93@gmail.com>
2021-12-03 00:02:33 -0700

    Complete warm up and meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name,
+    price,
+    type,
+  };
+}
+
+function addIngredients(string, ingredients) {
+  if(ingredients.indexOf(string) !== -1){
+    return;
+  } else{
+    return ingredients.push(string);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
